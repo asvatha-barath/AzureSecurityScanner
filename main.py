001277e2cfc99bf2b172e1381ac60d4940c176b0
Asvatha Barath <asvathabarath@gmail.com>
2026-08-12 20:50:33 -0400

    print test

--- main.py
+++ main.py
@@ -2,4 +2,13 @@ import json #importing json files
 with open ("resources.json", "r") as datafile:
     data = json.load(datafile) 
 
-print(data)
+
+resources = data["resources"]
+print("-"*45)
+for resource_type,resource_list in resources.items():
+    print(resource_type)
+    for resource in resource_list:
+        name = (resource["common_meta_data"])["name"]
+        id = (resource["common_meta_data"])["resource_id"]
+        print(" ",name," ", id)
+print("-"*45)
