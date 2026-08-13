58cd09cb9031375f7e52e335e7272e0d4b22c03f
Asvatha Barath <asvathabarath@gmail.com>
2026-08-12 20:58:17 -0400

    testing access to properties from .json

--- main.py
+++ main.py
@@ -10,5 +10,13 @@ for resource_type,resource_list in resources.items():
     for resource in resource_list:
         name = (resource["common_meta_data"])["name"]
         id = (resource["common_meta_data"])["resource_id"]
-        print(" ",name," ", id)
+        properties =(resource["properties"])
+        print(name," ", id)
+        for property,value in properties.items():
+            print(property, "----", value )
+
+  
+        print("-"*45)
+        
+        
 print("-"*45)
