8921c35c2c0a1133a630b406c01854de6885c6ec
Asvatha Barath <asvathabarath@gmail.com>
2026-08-12 21:02:01 -0400

    print test formatting

--- main.py
+++ main.py
@@ -4,19 +4,21 @@ with open ("resources.json", "r") as datafile:
 
 
 resources = data["resources"]
-print("-"*45)
+
 for resource_type,resource_list in resources.items():
+    print("-"*45)
     print(resource_type)
     for resource in resource_list:
         name = (resource["common_meta_data"])["name"]
         id = (resource["common_meta_data"])["resource_id"]
         properties =(resource["properties"])
-        print(name," ", id)
+        print(" ","-"*45)
+        print(" ",name," ", id)
+        print(" ","-"*45)
         for property,value in properties.items():
-            print(property, "----", value )
+            print(" ",property, "<---->", value )
 
   
-        print("-"*45)
         
         
-print("-"*45)
+
