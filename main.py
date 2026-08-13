73462e787413c2d6ac23dffb652c1f7f0940ce2c
Asvatha Barath <asvathabarath@gmail.com>
2026-08-12 21:23:34 -0400

    print public access key status with conditional

--- main.py
+++ main.py
@@ -12,11 +12,24 @@ for resource_type,resource_list in resources.items():
         name = (resource["common_meta_data"])["name"]
         id = (resource["common_meta_data"])["resource_id"]
         properties =(resource["properties"])
-        print(" ","-"*45)
-        print(" ",name," ", id)
-        print(" ","-"*45)
+        print("-"*45)
+        print("  name:",name," resource_id:", id)
         for property,value in properties.items():
-            print(" ",property, "<---->", value )
+            print(" ",property, "-->", value )
+
+        if resource_type == "virtual_machines" or resource_type == "databases": 
+                PAK = properties["security"]["public_access"]
+        elif resource_type=="storage_accounts": 
+            PAK = properties["network"]["public_network_access"]
+        else:
+             raise KeyError
+        if PAK == True:
+             print(f" {name} --> Public Access Key ENABLED")
+        else:
+             print(f" {name} --> Public Access Key NOT ENABLED")
+             
+                
+        
 
   
         
