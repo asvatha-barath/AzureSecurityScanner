cccffb3f09fb02f3bfbaf44bc5733bfa33d11c11
Asvatha Barath <asvathabarath@gmail.com>
2026-08-16 11:08:34 -0400

    adding comments

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 import json #importing json files
 def access_test_data():
+    #opens .json file for data
     with open ("resources.json", "r") as datafile:
      data = json.load(datafile) 
     return data
 
 def get_resource_details(resource):
+          #retrieves details from .json through indexing
           name = (resource["common_meta_data"])["name"]
           id = (resource["common_meta_data"])["resource_id"]
           properties =(resource["properties"])
           return name,id,properties
 
 def print_resource_details(name,id,properties,resource_type):
+        #prints all resource details. Not used in test but can be used when needed
         print("-"*45)
         print(f"resource type: {resource_type} name: {name} resource_id:, {id}")
 
@@ -20,6 +23,7 @@ def print_resource_details(name,id,properties,resource_type):
 
 
 def get_encryption(resource_type,properties):
+      #retrieves encryption status from .json
       if resource_type in ["storage_accounts","databases"]:
             ENCRYPT = properties["security"]["encryption"]
       elif resource_type=="virtual_machines": 
@@ -42,6 +46,7 @@ def get_public_access(resource_type,properties):
 
 
 def get_secure_boot(resource_type,properties):#USE WITH VM ONLY
+       #retrives secure boot status from virtual machines
        if resource_type== "virtual_machines":
           BOOT = properties["security"]["secure_boot"]
           return BOOT
@@ -65,15 +70,14 @@ def check_status(issue_list):
 
 
 data = access_test_data()
-issuedResources =  0
-resourcecount = 0
-totalissues = 0
+issuedResources =  0 #amount of resources that need attentions
+resourcecount = 0 #amount of resources read
+totalissues = 0 # total amount of issues
 
 
-resources = data["resources"]
+resources = data["resources"] #retreiving resources info from data file
 
-all_issues = {}
-all_status = {}
+all_issues = {} #dictionary to save issue_list outside the loop
 
 
 for resource_type, resource_list in resources.items():
@@ -88,17 +92,21 @@ for resource_type, resource_list in resources.items():
               if resource_type == "virtual_machines":
                 BOOT = get_secure_boot(resource_type, properties)
                 check_security(BOOT,True,issue_list,"SECURE BOOT")
-              
+              if issue_list:
+               issuedResources += 1
+               items = len(issue_list)
+               totalissues += items
               all_issues[name]= issue_list
 
-
+#printing header with counts
 print("="*45)
-print("Azure Security Agent")
+print("Azure Security Scan")
 print("="*45)
 print(f"Resources Scanned: {resourcecount}")
 print(f"Resources with Security Issues: {issuedResources}")
 print(f"Total Security Issues: {totalissues}")
 
+#loop to print issue list and alerts for each resources
 for resource_type, resource_list in resources.items():
         for resource in resource_list:
               name,id,properties = get_resource_details(resource)
@@ -111,9 +119,6 @@ for resource_type, resource_list in resources.items():
               if not issue_list:
                     print("ALL SECURITY TESTS PASSED")
               else:
-                 issuedResources += 1
-                 items = len(issue_list)
-                 totalissues += items
                  for issueTest in issue_list:
                       print(f"FAILED:{issueTest} TEST")
 
